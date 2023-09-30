75ed9aa6e7601c9d06b33382c3f13b7f697c1241
linuxmobile <bdiez19@gmail.com>
2023-09-30 19:49:58 -0300

    feat: added sociallinks section

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,15 @@
+export const socialLinks = [
+  {
+    label: "github",
+    url: "https://github.com"
+  },
+  {
+    label: "twitter",
+    url: "https://twitter.com"
+  },
+  {
+    label: "linkedin",
+    url: "https://linkedin.com",
+    expanded: true
+  }
+]
